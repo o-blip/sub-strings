a7bbfd73992388528afae8019224f8e0d1493d72
piero <dolibiroli@gmail.com>
2022-03-17 10:46:54 -0500

    finished program

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,18 +1,29 @@
 def substrings(string, dictionary)
-    substr_count = Hash.new(0)
-    str_array = string.downcase.split('') # want case-insensitive
-    p str_array
-    filtered_array = str_array.select{|letter| (97..122) === letter.ord}
-    filtered_str = filtered_array.join
-
+    word_count = Hash.new(0)
+    filtered_str = filter_string(string)
 
     dictionary.each do |word|
-        if filtered_str.include?(word)
-            substr_count[word] += 1
-        end
+        check_array = create_substr(filtered_str, word.length)
+        word_check = word.chars
+        word_count[word] = check_array.count(word_check)
     end
-    return substr_count
+    
+    word_count.delete_if{|word, count| count == 0}
+    return word_count
 end
+
+
+def filter_string(string)
+    # filters out symbols and creates array of lower-case characters
+    str_array = string.downcase.split('')
+    filtered_array = str_array.select{|letter| (97..122) === letter.ord || 32 == letter.ord} # ASCII code for letters: 97-122
+end
+
+def create_substr(char_array, length)
+    # creates array of arrays of specified length with consecutive characters
+    sub_strings = char_array.each_cons(length).to_a
+end
+
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 p substrings("Howdy partner, sit down! How's it going?", dictionary)
